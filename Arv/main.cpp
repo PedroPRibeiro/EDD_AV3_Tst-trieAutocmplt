82a19601ef9c27669b2049a20bcb39a63a175e8d
PedroPRibeiro <57674709+PedroPRibeiro@users.noreply.github.com>
2019-12-13 00:33:04 -0300

    Update main.cpp
    MUDOU FOI TUDO

--- Arv/main.cpp
+++ Arv/main.cpp
@@ -7,95 +7,118 @@
 #include <fstream>
 
 using namespace std;
-#define ALFABETO (57)
+#define ALFABETO (255)
 
 struct Arv {
-	char gambiarra;
-	//struct Arv* esq;
-	//struct Arv* dir;
-	unsigned fim: 1;
-
-
+	char c;
+	bool fim;
+	int x;
 	struct Arv *esq, *mei, *dir;
 };
 
-Arv* arv_criavazia (void){
-	return NULL;
-}
+union Generico{//criar uma arvore com nome arv, com o intuito de passar valor = NULL no main, sem quebrar o codigo
+    Arv *arv;
+};
 
-Arv* arv_cria (char gambiarra, Arv* sae, Arv* sam, Arv* sad){
+Arv* arv_cria (char c, unsigned fim){
 	Arv* p=(Arv*)malloc(sizeof(Arv));
-	p->gambiarra= gambiarra;
-	p->esq = sae;
-	p->mei = sam;
-	p->dir = sad;
-	p->fim = 0;
+	p->c= c;
+	p->fim = fim;
+	p->x=0;
+	p->esq = p->mei = p->dir = NULL;
 	return p;
 }
 
-int arv_vazia (Arv* a){
-	return a==NULL;
-}
-
-Arv* arv_libera (Arv* a){
-	if (!arv_vazia(a)){
-	  arv_libera (a->esq); /* libera sae */
-	  arv_libera (a->dir); /* libera sad */
-	  free(a); /* libera raiz */
-	}
-	return NULL;
-}
-
-bool arv_pertence (Arv* a, int c){
-  if (arv_vazia(a))
-	return false; /* árvore vazia: não encontrou */
-  bool p = a->gambiarra==c;
-  if( p ) return p;
-  p = arv_pertence (a->esq,c);
-  if( p ) return p;
-  return arv_pertence (a->dir,c);
-}
-
-
-
-
-void adicona_arvore(Arv** a, char *palavra){
-        if (arv_vazia(*a))
-            *a = arv_cria(*palavra,arv_criavazia(),arv_criavazia(),arv_criavazia());
-        else if ((*a)->gambiarra > *palavra)
-            adicona_arvore(&((*a)->esq),palavra);
-
-        else if((*a)->gambiarra < *palavra)
-            adicona_arvore(&((*a)->dir),palavra);
-
-        else
-        {
-            if (*(palavra+1))
-                adicona_arvore(&((*a)->mei), palavra+1);
-            else
-                (*a)->fim = 1;
-        }
-
-}
-
-int busca(struct Arv *a, char *palavra)
-{
-    if (!a)
+int altura(Arv *a){
+    if(a == NULL) return -1;
+    if(a->esq == NULL && a->dir == NULL){
         return 0;
-
-    if (*palavra < (a)->gambiarra)
-        return busca(a->esq, palavra);
-
-    else if (*palavra > (a)->gambiarra)
-        return busca(a->dir, palavra);
-
-    else
-    {
-        if (*(palavra+1) == '\0')
-            return a->fim;
-
-        return busca(a->mei, palavra+1);
+    }else{
+        if(altura(a->esq) > altura(a->dir)) return 1 + altura(a->esq);
+        else return 1 + altura(a->dir);
     }
+}
+
+int nivelBalanceamento(Arv *a){
+    if(a == NULL) return 0;
+    if(a->esq == NULL && a->dir == NULL) return 0;
+    return altura(a->esq) - altura(a->dir);
+}
+
+Arv **arvBuscaComBal(Arv **a){
+    if(*a == NULL || (*a)->x == 3) return NULL;
+
+    if((*a)->esq != NULL && (*a)->x == 0){
+        Arv **tmp = arvBuscaComBal(&(*a)->esq);
+        if(tmp != NULL) return tmp;
+    }
+    (*a)->x++;
+
+    if((*a)->dir != NULL && (*a)->x == 1){
+        Arv **tmp = arvBuscaComBal(&(*a)->dir);
+        if(tmp != NULL) return tmp;
+    }
+    (*a)->x++;
+
+    if(nivelBalanceamento(*a) > 1 || nivelBalanceamento(*a) < -1) return a;
+
+    if((*a)->mei != NULL && (*a)->x == 2){
+        Arv **tmp = arvBuscaComBal(&(*a)->mei);
+        if(tmp != NULL) return tmp;
+    }
+    (*a)->x++;
+
+    return NULL;
+}
+
+
+void adicionar(Arv** arv, const char* palavra){
+    if(*arv == NULL){
+        *arv = arv_cria(palavra[0], palavra[1] == '\0');
+        if (palavra[1] == '\0') return;
+    }
+
+    if(palavra[0] == (*arv)->c){
+        adicionar(&(*arv)->mei, palavra+1);
+    }else  if(palavra[0] > (*arv)->c){
+        adicionar(&(*arv)->dir, palavra);
+    }else{
+        adicionar(&(*arv)->esq, palavra);
+    }
+}
+void adicionarOGenerico(int type, Generico* arv, const char* palavra){
+            adicionar(&(arv->arv), palavra);
+
+}
+
+
+void mostrar(Arv *a, string tmp, int *i){
+    if(a == NULL || *i >= 10) return;
+    mostrar(a->esq, tmp, i);
+
+    tmp.push_back(a->c);
+    if(a->fim==true){
+        cout << tmp << endl;
+    }
+    mostrar(a->mei, tmp, i);
+
+    tmp.pop_back();
+    mostrar(a->dir, tmp, i);
+}
+
+void buscaArv(const char *word, Arv *a, string tmp){
+    if (a->c == word[0] && word[1] == '\0'){
+        tmp.push_back(a->c);
+        if(a->fim) cout << tmp << endl;
+        int i = 0;
+        mostrar(a->mei, tmp, &i);
+    }
+    else if (a->c == word[0]){
+        tmp.push_back(a->c);
+        word++;
+        buscaArv(word, a->mei, tmp);
+    }else if(word[0] < a->c) buscaArv(word, a->esq, tmp);
+     else if(word[0] > a->c) buscaArv(word, a->dir, tmp);
 }
 
 void gotoxy(int x, int y){
@@ -103,108 +126,51 @@ void gotoxy(int x, int y){
      SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),(COORD){x-1,y-1});
 }
 
-void sugestao(struct Arv* a, string gambiarraPt2)
-{
-
-    //procurando complemento com o prefixo fornecido
-    if (a->fim=1)
-    {
-        cout << gambiarraPt2;
-        cout << endl;
-    }
-    if (a->fim=0)
-        return;
-
-    for (int i = 0; i < ALFABETO; i++)
-    {
-        if (a->mei)
-        {
-           // acrescenta o caractere atual à string prefixo
-            gambiarraPt2.push_back(65 + i);
-            sugestao(a->mei, gambiarraPt2);
-            gambiarraPt2.pop_back();
-        }
-    }
-}
-
-int imprimir(Arv* a, const string j)
-{
-        sugestao(a,j );
-        return 1;
-
-}
-
-
-
-
-
-void listaComPrefixo(Arv *a, string gambiarraPt2){
-    gotoxy(1,5);
-    sugestao(a,gambiarraPt2);
-    gotoxy(1,2);
-}
 
 
 int main()
 {
+    clock_t tempo = clock();
 
+    Generico myArv;
+    myArv.arv = NULL;
 
-    clock_t t1, t2, t3;
-
-    Arv *a = arv_criavazia();
-
-
-    ifstream dicionario;
-    dicionario.open("words.txt");
     string saida;
 
-    if (dicionario.is_open()){
-
-        while (getline(dicionario, saida)){
-            char p[saida.length()];
-            strcpy(p, saida.c_str());
-            adicona_arvore(&a,p);
-            cout<< p <<endl;
-
-            t2=clock();
-            busca(a, p);
-
+    tempo = clock();
+    ifstream dicionario("wordds.txt");
+    if(dicionario.is_open()){
+        while(getline(dicionario, saida)){
+            adicionarOGenerico(0, &myArv, saida.c_str());
         }
-
     }
 
-    dicionario.close();
-    t3 = difftime(t1,t2);
-    cout<<"Tempo: "<<((float)t3)/CLOCKS_PER_SEC<<"segundos "<<endl;
+    Arv **a;
+    tempo = clock();
+    do{
+    a = arvBuscaComBal(&myArv.arv);
+    }while(a != NULL);
 
-
-
-    t1 = clock();
-
+    string tmp = "";
     char c;
-    string gambiarraPt2 = "";
-
-    char j[gambiarraPt2.length()];
-    strcpy(j, gambiarraPt2.c_str());
-
-    char *palavra = j;
-
+    string s = "";
 
     while (c != 13){
-        system ("CLS");
-        listaComPrefixo(a,palavra);
-        cout<<"Tempo de pesquisa : "<<((float)t3)/CLOCKS_PER_SEC<<"segundos "<<endl;
-        printf("%d",c);
+        gotoxy(5,1);
+        printf("%d    ", c);
         cout << "Texto: ";
-        cout << gambiarraPt2;
+        cout << s;
         c = getche();
-        if (c == 8 && gambiarraPt2.length()>0){
-            gambiarraPt2.pop_back();
+        system ("CLS");
+        if (c == 8 && s.length()>0){
+            s.pop_back();
         }
-        else if (c >= 65 && c <= 122){
-            gambiarraPt2.push_back(c);
+        else if (c >= 10 && c <= 255){
+            s.push_back(c);
         }
+        tempo = clock();
+        if(s != "") buscaArv(s.c_str(), myArv.arv, tmp);
+        gotoxy(25,1);cout << "Tempo: " << float(clock() - tempo) / CLOCKS_PER_SEC << endl;
     }
-
     return 0;
 }
